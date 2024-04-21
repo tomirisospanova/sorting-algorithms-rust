ba78caf2325c819aff43f46790b3745169bd017e
tomirisospanova <124910398+tomirisospanova@users.noreply.github.com>
2024-04-21 23:04:27 +0500

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,20 @@
-extern crate sorting_library; // Import the sorting library
-
-use sorting_library::{quick_sort, selection_sort, insertion_sort, merge_sort};
+use sorting_library::sorting::*;
 
 fn main() {
-    let mut numbers = vec![10, 7, 15, 6];
-    println!("Original: {:?}", numbers);
+    let mut characters = vec!['u', 'b', 's', 'l'];
+    merge_sort(&mut characters);
+    println!(" Merge Sort: {:?}", characters);
 
-    // Quick Sort
-    let mut numbers_quick = numbers.clone();
-    quick_sort(&mut numbers_quick);
-    println!("Quick Sort: {:?}", numbers_quick);
+    let mut vec = vec![67,38 ,99, 78, 65];
+    quick_sort(&mut vec);
+    println!(" Quick Sort: {:?}", vec);
 
-    // Selection Sort
-    let mut numbers_selection = numbers.clone();
-    selection_sort(&mut numbers_selection);
-    println!("Selection Sort: {:?}", numbers_selection);
+    let mut floats = vec![5.67, 6.45, 6.78, 9.78, 3.78];
+    selection_sort(&mut floats);
+    println!("Selection Sort: {:?}", floats);
 
-    // Insertion Sort
-    let mut numbers_insertion = numbers.clone();
-    insertion_sort(&mut numbers_insertion);
-    println!("Insertion Sort: {:?}", numbers_insertion);
-
-    // Merge Sort
-    let mut numbers_merge = numbers.clone();
-    merge_sort(&mut numbers_merge);
-    println!("Merge Sort: {:?}", numbers_merge);
+    let mut words = vec!["blockchain".to_string(), "tomi".to_string(), "dragon".to_string()];
+    insertion_sort(&mut words);
+    println!("Insertion Sort: {:?}", words);
+    
 }
